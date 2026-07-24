9288dc513122380bd4e00a619a97da0f1e0ae5f8
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-24 15:50:31 -0300

    feat(js): add target event click

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-const buttons = document.querySelectorAll("#image-picker li")
-const image = document.querySelector("#product-img")
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-img");
 
 buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log(e);
+    // btn.classList.add("selected");
 
-    btn.addEventListener("click", () =>{
+    buttons.forEach((btn) => {
+      btn.querySelector(".color").classList.remove("selected");
+    });
 
+    const buttonSelected = e.target;
 
+    const id = buttonSelected.getAttribute("id");
 
-    })
+    console.log(id);
 
-})
+    buttonSelected.querySelector(".color").classList.add("selected");
+  });
+});
