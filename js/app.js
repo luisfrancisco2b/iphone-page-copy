8a50806518079fc987b9dfa15b1db845bcd3c980
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-24 13:13:06 -0300

    feat(js): add inital js code

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll("#image-picker li")
+const image = document.querySelector("#product-img")
+
+buttons.forEach((btn) => {
+
+    btn.addEventListener("click", () =>{
+
+
+
+    })
+
+})
